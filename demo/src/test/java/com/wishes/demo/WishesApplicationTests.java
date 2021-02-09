8175f05e4b2ce5df6df8457f88b75548cc44263d
MxmStm <you@example.com>
2021-02-09 23:05:04 +0300

    default commit

---
+++ demo/src/test/java/com/wishes/demo/WishesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishes.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
