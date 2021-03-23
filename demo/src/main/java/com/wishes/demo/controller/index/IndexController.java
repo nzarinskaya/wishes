8702d4bafb445047fe9089ef5b1c91508fb332bb
MxmStm <you@example.com>
2021-03-24 02:45:11 +0300

    add controller

--- demo/src/main/java/com/wishes/demo/controller/index/IndexController.java
+++ demo/src/main/java/com/wishes/demo/controller/index/IndexController.java
@@ -1,6 +1,8 @@
 package com.wishes.demo.controller.index;
 
 import com.wishes.demo.controller.AbstractController;
+import com.wishes.demo.service.user.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,5 +20,20 @@ public class IndexController extends AbstractController {
     public String index() {
         return "redirect:/main";
     }
+
+    @GetMapping("/main")
+    public ModelAndView login(@RequestParam Optional<String> error) {
+        final ModelAndView modelAndView = new ModelAndView("main");
+        if (error.isPresent()) {
+            modelAndView.addObject("errorMessage", "Error occurred");
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "/registration/registration";
+    }
+
 
 }
--- demo/src/main/java/com/wishes/demo/controller/profile/ProfileContr.java
+++ demo/src/main/java/com/wishes/demo/controller/profile/ProfileContr.java
@@ -1,12 +1,32 @@
 package com.wishes.demo.controller.profile;
 
 import com.wishes.demo.controller.AbstractController;
+import com.wishes.demo.model.user.UserModel;
+import com.wishes.demo.service.auth.dto.UserDetailsDTO;
+import com.wishes.demo.service.user.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/profile")
 public class ProfileContr extends AbstractController {
 
+    UserServiceImpl userService;
 
+    @GetMapping()
+    public ModelAndView profile(Authentication authentication) {
+        UserModel user = ((UserDetailsDTO) authentication.getPrincipal()).getUserModel();
+
+        ModelAndView mav = new ModelAndView("/profile", "user", user);
+        return mav;
+
+    }
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
+    }
 }
